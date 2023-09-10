d04a32e1ba1b0769e2ac30d10b89dbeb28ac187c
Daniel Novais <danlicesol@gmail.com>
2023-09-10 05:19:27 -0300

    ajuste responsivos

--- src/app/data/datafake.ts
+++ src/app/data/datafake.ts
@@ -7,20 +7,20 @@ export const dataFake = [
 },
 {
     "id":"2",
-    "title":"eee",
-    "description":"",
+    "title":"Os Computadores da Década de 1980: Limitações e Avanços Tecnológicos",
+    "description":"Na década de 1980, os computadores ainda eram relativamente novos e limitados em suas capacidades. Eles foram usados ​​principalmente para processamento de texto, planilhas e programação básica. A tecnologia da época era muito mais lenta e menos poderosa do que hoje. Os computadores tinham memória e armazenamento limitados e o poder de processamento era muito mais lento. Os computadores mais populares da época eram o Apple II, o Commodore 64 e o IBM PC. Esses computadores usavam disquetes para armazenamento e tinham recursos gráficos limitados. A internet ainda estava engatinhando e a maioria das pessoas nunca tinha ouvido falar dela.",
     "photoCover":"https://idocode.com.br/wp-content/uploads/2016/10/8-768x581-1.jpg"
 },
 {
     "id":"3",
-    "title":"eee",
-    "description":"",
+    "title":"A tecnologia avançando em pouco tempo.",
+    "description":"Na década de 2000, os computadores eram usados ​​para uma variedade de tarefas, desde processamento básico de texto e navegação na web até tarefas mais complexas, como edição de vídeo e renderização 3D. A tecnologia da época era muito mais limitada do que é hoje, com computadores com processadores muito mais lentos e menos RAM. Os discos rígidos também eram muito menores, com um disco rígido médio em torno de 40 GB. A internet também era muito mais lenta, sendo as conexões dial-up a forma mais comum de acessar a web. As placas gráficas também eram muito menos poderosas, com a maioria dos computadores possuindo placas gráficas integradas que só podiam lidar com jogos básicos.",
     "photoCover":"https://i.pinimg.com/236x/9a/cd/bc/9acdbcf3569c1137ec49288882d0bfcd.jpg"
 },
 {
     "id":"4",
-    "title":"eee",
-    "description":"",
+    "title":"O avanço tecnologico..",
+    "description":"A tecnologia moderna é mais avançada do que nunca. Estamos vivendo em um mundo cada vez mais conectado, onde as pessoas podem se comunicar e compartilhar informações de forma instantânea. Os dispositivos móveis, como smartphones e tablets, tornaram-se cada vez mais populares, permitindo que as pessoas acessem a internet em qualquer lugar. A inteligência artificial e a realidade aumentada também estão se tornando cada vez mais populares, permitindo que as pessoas interajam com o mundo digital de maneiras inovadoras. Além disso, a tecnologia está sendo usada para melhorar a saúde, a educação e a segurança, tornando o mundo um lugar melhor para viver.",
     "photoCover":"https://cbltech.com.br/uploads/7c120c1967f0176ffb58762831b66abb1550175431-.jpg"
 }
 ]
